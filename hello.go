aa5ca56f78f9a5d24d535cf1440b323f29bbb3e8
obzva <dngyng1000@gmail.com>
2025-03-10 17:40:23 -0700

    [feat] Hello() accepts language parameter

--- hello.go
+++ hello.go
@@ -2,10 +2,36 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
-	return "Hello, " + name
+const (
+	defaultName = "World"
+
+	korean = "Korean"
+	french = "French"
+
+	englishHelloPrefix = "Hello, "
+	koreanHelloPrefix  = "안녕, "
+	frenchHelloPrefix  = "Bonjour, "
+)
+
+func Hello(name, language string) string {
+	if name == "" {
+		name = defaultName
+	}
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case korean:
+		prefix = koreanHelloPrefix
+	case french:
+		prefix = frenchHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
