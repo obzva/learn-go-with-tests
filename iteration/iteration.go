9e89fd710a382b96fc002eca3c707abe7f5d4c6f
obzva <dngyng1000@gmail.com>
2025-03-10 22:45:26 -0700

    iteration and benchmarks

---
+++ iteration/iteration.go
@@ -0,0 +1,11 @@
+package iteration
+
+import "strings"
+
+func Repeat(character string, count int) string {
+	var repeated strings.Builder
+	for range count {
+		repeated.WriteString(character)
+	}
+	return repeated.String()
+}
---
+++ iteration/iteration_test.go
@@ -0,0 +1,27 @@
+package iteration
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a", 10)
+	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func BenchmarkRepeat(b *testing.B) {
+	for b.Loop() {
+		Repeat("a", 10)
+	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 7)
+	fmt.Println(repeated)
+	// Output: aaaaaaa
+}
